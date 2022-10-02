ce433269c37aeb4083c58ce74db40513bf358535
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-10-02 23:04:43 +0200

    feat: :fire: Parsing PDF array into API-friendly object
    Signed-off-by: Mateusz Tatko <60148744+mtatko@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import { getPDFsInBase64 } from './middleware/documentParsing.js';
+
 //todo: obtain pdfs as base64 and send to api
+const main = async () => {
+  const obj: { [index: string]: string } = {};
+  const pdfs = await getPDFsInBase64();
+  for (const schoolClass of pdfs) {
+    obj[schoolClass.className] = schoolClass.timetable;
+  }
+  //todo: send to api
+};
+
+main();
